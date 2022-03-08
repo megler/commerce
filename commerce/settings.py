42e3be532fd18c22e988b011ba8846394afd492b
megler <majeresmom@att.net>
2022-03-08 13:00:51 -0500

    changed secret key again

--- commerce/settings.py
+++ commerce/settings.py
@@ -14,7 +14,7 @@ import os
 from django.contrib.messages import constants as messages
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(path.join(basedir, ".env"))
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["PASSWORD"]
+SECRET_KEY = environ.get["PASSWORD"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
