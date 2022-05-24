d1dcbca915a391df8ade96dcc89e4ec8f173b8b3
megler <majeresmom@att.net>
2022-05-23 20:01:56 -0400

    update heroku

--- commerce/settings.py
+++ commerce/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "https://accio-auctions.herokuapp.com/", "127.0.0.1", "localhost"
